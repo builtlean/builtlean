5ed2a4db4505a422be617b9ac7c221c7dc23a9d9
stonedev <email@tinastone.com>
2013-03-27 00:37:32 -0300

    Update header_orderform.php
    Set header file to be used with InfusionSoft order forms

--- header_orderform.php
+++ header_orderform.php
@@ -5,14 +5,9 @@
 <title>Fitness Tips to Get Lean, Toned, &amp; Lose Fat | BuiltLean.com</title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="http://www.builtlean.com/wp-content/themes/builtlean/styles/styledcart.css" />
-<?php wp_head(); ?>
-<?php if(is_page('12970') == 1) { 
-  $temp_uri=get_template_directory_uri();
-	$temp_uri=str_replace('cdn','www',$temp_uri);
-	?>
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_url_uri();?>/styles/styledcart.css" />
 <script type="text/javascript">(function() {
-                        var styleArray = ["http://www.builtlean.com/wp-content/themes/builtlean/styles/styledcart.css"];
+                        var styleArray = ["<?php echo get_template_url_uri();?>/styles/styledcart.css"];
                         if (window.Infusion) {
                             Infusion.stylesLoaded(styleArray);
                         } else if (window.InfusionStyles) {
@@ -22,38 +17,21 @@
                         }
 
                     })();</script>
-<link type="text/css" rel="stylesheet" href="http://www.builtlean.com/wp-content/themes/builtlean/styles/cssSnippet.jsp" />
-<link type="text/css" rel="stylesheet" href="http://www.builtlean.com/wp-content/themes/builtlean/styles/cssSnippet(1).jsp" />
-<script type="text/javascript" src="http://www.builtlean.com/wp-content/themes/builtlean/js/sink_jq.jsp"></script>
-<script type="text/javascript" src="http://www.builtlean.com/wp-content/themes/builtlean/js/sink_js.jsp"></script>
-<link type="text/css" rel="stylesheet" media="all" href="http://www.builtlean.com/wp-content/themes/builtlean/styles/lightbox.css" id="lazy-loaded-1363052028412" />
-<script src="http://www.builtlean.com/wp-content/themes/builtlean/js/jquery-qtip-1.0.0-rc3.js" type="text/javascript"></script>
-<script src="http://www.builtlean.com/wp-content/themes/builtlean/js/orderForms.js" type="text/javascript"></script>
-<script src="http://www.builtlean.com/wp-content/themes/builtlean/js/checkoutHiddenFields.js" type="text/javascript"></script>
+<link type="text/css" rel="stylesheet" href="<?php echo get_template_url_uri();?>/styles/cssSnippetjsp.css" />
+<link type="text/css" rel="stylesheet" href="<?php echo get_template_url_uri();?>/styles/cssSnippet1jsp.css" />
+<script type="text/javascript" src="<?php echo get_template_url_uri();?>/js/sink_jq.jsp"></script>
+<script type="text/javascript" src="<?php echo get_template_url_uri();?>/js/sink_js.jsp"></script>
+<link type="text/css" rel="stylesheet" media="all" href="<?php echo get_template_url_uri();?>/styles/lightbox.css" id="lazy-loaded-1363052028412" />
+<script src="<?php echo get_template_url_uri();?>/js/jquery-qtip-1.0.0-rc3.js" type="text/javascript"></script>
+<script src="<?php echo get_template_url_uri();?>/js/orderForms.js" type="text/javascript"></script>
+<script src="<?php echo get_template_url_uri();?>/js/checkoutHiddenFields.js" type="text/javascript"></script>
 
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
-<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.10.0/jquery.validate.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/priceformat.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/numOnly.js"></script>
 
 </head>
 
 <body <?php body_class(); ?>>
 <?php if(!is_page('12970')) { ?>
-<div id="primary-nav" style="height:65px">
-<div style="margin:20px auto; width:969px;">
- <div><img src="http://www.builtlean.com/wp-content/themes/builtlean/images/logo_landingpage.jpg" width="192" height="26" alt="BuiltLean.com"/></div> 
-          
-           
-</div>
-
-</div><!--#primary-nav-->
 <div class="wrap">
 <?php } else { ?>
 <div id="wrapper">
-        <!--start header-->
-        <div id="header">
-            <img src="http://www.builtlean.com/wp-content/themes/builtlean/images/logo_order.png" alt="" />
-        </div><!--end header-->
-
 <?php } ?>
